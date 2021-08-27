1263a923f7fb9265c2d29bfbb324efb8f42f72eb
Matthew Barros <matthewbarros@Matthews-MacBook-Pro.local>
2021-08-26 23:36:32 -0500

    rremoved extraneous comments

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
+var playerName = window.prompt("What is your player's name?");
+console.log(playerName);
+
 window.alert("This is an alert! JavaScript is running!");
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight();
